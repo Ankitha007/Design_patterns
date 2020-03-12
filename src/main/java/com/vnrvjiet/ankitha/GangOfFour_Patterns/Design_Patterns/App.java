d48cd982bfc774ec379fa2eae1fc01cf611238fc
Ankitha <vankithareddy07@gmail.com>
2020-03-12 22:19:08 +0530

    first

---
+++ src/main/java/com/vnrvjiet/ankitha/GangOfFour_Patterns/Design_Patterns/App.java
@@ -0,0 +1,13 @@
+package com.vnrvjiet.ankitha.GangOfFour_Patterns.Design_Patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/vnrvjiet/ankitha/GangOfFour_Patterns/Design_Patterns/AppTest.java
@@ -0,0 +1,20 @@
+package com.vnrvjiet.ankitha.GangOfFour_Patterns.Design_Patterns;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
